75b13a5d1da178335c1b2fcca00c6a673fa4d8c0
JLeson <joelburleson0104@gmail.com>
2023-07-12 17:49:50 +0200

    fixed naming conflict

--- src/utils/is_builtin.c
+++ src/utils/is_builtin.c
@@ -3,35 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   is_builtin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsarkoh <fsarkoh@student.42.fr>            +#+  +:+       +#+        */
+/*   By: joel <joel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 15:52:07 by fsarkoh           #+#    #+#             */
-/*   Updated: 2023/07/12 15:58:32 by fsarkoh          ###   ########.fr       */
+/*   Updated: 2023/07/12 17:49:28 by joel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_bool	strcmp(char *s1, char *s2)
+static t_bool	ft_strcmp(char *s1, char *s2)
 {
 	return (!ft_strncmp(s1, s2, ft_max(ft_strlen(s1), ft_strlen(s2))));
 }
 
 t_bool	is_builtin(char *cmd)
 {
-	if (strcmp(cmd, "exit"))
+	if (ft_strcmp(cmd, "exit"))
 		return (TRUE);
-	if (strcmp(cmd, "pwd"))
+	if (ft_strcmp(cmd, "pwd"))
 		return (TRUE);
-	if (strcmp(cmd, "cd"))
+	if (ft_strcmp(cmd, "cd"))
 		return (TRUE);
-	if (strcmp(cmd, "env"))
+	if (ft_strcmp(cmd, "env"))
 		return (TRUE);
-	if (strcmp(cmd, "export"))
+	if (ft_strcmp(cmd, "export"))
 		return (TRUE);
-	if (strcmp(cmd, "unset"))
+	if (ft_strcmp(cmd, "unset"))
 		return (TRUE);
-	if (strcmp(cmd, "echo"))
+	if (ft_strcmp(cmd, "echo"))
 		return (TRUE);
 	return (FALSE);
 }
