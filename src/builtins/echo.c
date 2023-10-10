1768ee281c7667a79ed3e34593963c766474e0c6
JLeson <joelburleson0104@gmail.com>
2023-10-10 16:12:05 +0200

    fixed mistake in echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsarkoh <fsarkoh@student.42.fr>            +#+  +:+       +#+        */
+/*   By: joel <joel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 19:42:11 by joel              #+#    #+#             */
-/*   Updated: 2023/08/23 15:35:10 by fsarkoh          ###   ########.fr       */
+/*   Updated: 2023/10/10 16:11:42 by joel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,17 @@ t_status	cmd_echo(char **args)
 {
 	unsigned int	cidx;
 	t_bool			has_newline;
+	t_bool			n_option;
 
 	cidx = 1;
 	has_newline = TRUE;
+	n_option = FALSE;
 	if (args[1])
-		has_newline = ft_strncmp(args[1], "-n", ft_max(ft_strlen(args[1]), 2));
-	if (!has_newline)
+	{
+		has_newline = strcmp(args[1], "-n");
+		n_option = !ft_strncmp(args[1], "-n", 2);
+	}
+	if (!has_newline || n_option)
 		cidx = 2;
 	while (args[cidx])
 	{
