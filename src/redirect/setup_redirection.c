150cbd75900e933590f7e7c99ab291516a200c8a
JLeson <joelburleson0104@gmail.com>
2023-10-11 16:08:22 +0200

    fixed file permissions when redirecting

--- src/redirect/setup_redirection.c
+++ src/redirect/setup_redirection.c
@@ -6,7 +6,7 @@
 /*   By: joel <joel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 15:50:42 by joel              #+#    #+#             */
-/*   Updated: 2023/10/10 17:34:08 by joel             ###   ########.fr       */
+/*   Updated: 2023/10/11 16:07:44 by joel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ static int	open_outfile(t_iofile *file)
 	else
 		flags |= O_TRUNC;
 	fd = ft_mopen(file->path, flags, 0644);
+	if (fd == -1)
+		print_err("writing to STDOUT instead of: ", file->path);
 	return (fd);
 }
 
@@ -33,6 +35,8 @@ static int	open_infile(t_iofile *file)
 	if (file->mode == HEREDOC_MODE)
 		return (create_heredoc(file));
 	fd = ft_open(file->path, O_RDONLY);
+	if (fd == -1)
+		print_err("reading from STDIN instead of: ", file->path);
 	return (fd);
 }
 
@@ -56,6 +60,8 @@ void	setup_redirect_out(t_cmd *cmd)
 	while (cmd->outfiles[file_idx + 1])
 	{
 		current_file = open_outfile(cmd->outfiles[file_idx]);
+		if (current_file == -1)
+			return ;
 		close(current_file);
 		file_idx++;
 	}
