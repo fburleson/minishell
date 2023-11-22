0253fa282856444d3e2270a714f64dfc4d18ef10
JLeson <joelburleson0104@gmail.com>
2023-11-22 20:37:05 +0100

    fixed leak

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: joel <joel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 15:30:23 by kaltevog          #+#    #+#             */
-/*   Updated: 2023/11/14 16:39:22 by joel             ###   ########.fr       */
+/*   Updated: 2023/11/22 20:35:01 by joel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,12 +65,15 @@ t_status	cmd_export(char **args, char ***env)
 {
 	char	**append_env;
 	char	**temp_env;
+	char	*raw_name;
 
 	if (!is_value_valid(args[1]))
 		return (ERROR);
 	if (!is_name_unique(args[1], *env))
 	{
-		temp_env = cp_remove(*env, raw_envvar_name(args[1]));
+		raw_name = raw_envvar_name(args[1]);
+		temp_env = cp_remove(*env, raw_name);
+		free(raw_name);
 		if (!temp_env)
 			return (ERROR);
 		free_strarray(*env);
